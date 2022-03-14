b6262c8b542d61e4e3b55f5a5e980fc1dcd1ec02
Nanotect <61177761+Adivise@users.noreply.github.com>
2022-03-15 04:36:56 +0700

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -4,14 +4,18 @@ module.exports = async (client, message) => {
     if(message.author.bot || message.channel.type === "dm") return;
 
     let prefix = client.prefix;
+	
+	if (!client.dev.includes(message.author.id) && client.dev.length > 0) { 
+		console.log(chalk.red(`[INFORMATION] ${message.author.tag} Trying request the command!`)); 
+		return;
+    }
 
     const mention = new RegExp(`^<@!?${client.user.id}>( |)$`);
-
-    if (message.content.match(mention)) {
-      message.channel.send(`**My prefix is \`${prefix}\`**`).then(msg => {
-        setTimeout(() => msg.delete(), 5000)
-    });
-    };
+	if (message.content.match(mention)) {
+		message.channel.send(`**My prefix is \`${prefix}\`**`).then(msg => {
+			setTimeout(() => msg.delete(), 5000)
+		});
+	};
 
     const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
     const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
@@ -22,17 +26,12 @@ module.exports = async (client, message) => {
     const command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
     if(!command) return;
 
-    if (!client.dev.includes(message.author.id) && client.dev.length > 0) { 
-      console.log(chalk.red(`[INFORMATION] ${message.author.tag} Trying request the command!`)); 
-      return;
-    }
-
-      try {
-          command.run(client, message, args, prefix)
+    try {
+			command.run(client, message, args, prefix)
       } catch (error) {
         console.log(error)
         message.channel.send({ content: 'Something went wrong.' }).then(msg => {
-          setTimeout(() => msg.delete(), 5000)
-      });
+			setTimeout(() => msg.delete(), 5000)
+		});
     }
-  }
+}
