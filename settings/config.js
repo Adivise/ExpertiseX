e6444c58bddab88375f65edcb4194b76f29a0c6e
Nanotect <61177761+Adivise@users.noreply.github.com>
2022-03-14 23:47:13 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -5,9 +5,6 @@ module.exports = {
     PREFIX: process.env.PREFIX || ".", //<= default is .  // bot prefix
 
     OWNER_ID: process.env.OWNER_ID || "YOUR_CLIENT_ID", //your owner discord id example: "515490955801919488" this is not id like *Stylish.#4078
-
-    LEAVE_TIMEOUT: parseInt(process.env.LEAVE_TIMEOUT || "120000"), // leave timeout default "120000" = 2 minutes // 1000 = 1 seconds
-
     DEV_ID: process.env.DEV_ID || ["515490955801919488", "908006641155309619"], // if you want to use command bot only, you can put your id here  // example: ["515490955801919488", "543595284345782296"]
 
     NODES: [
